ea25f8ab8cd9782334d27c7317e6478c8834fe9e
Nate Brennand <natebrennand@gmail.com>
2013-09-15 14:08:28 -0700

    flesh out app.py and add some documentation

--- src/app.py
+++ src/app.py
@@ -4,43 +4,62 @@ from flask import Flask, request
 from flask.ext.pymongo import PyMongo
 from functools import wraps
 
+########################################
 # controllers
+########################################
 from controllers import person as person_controller
 
+########################################
 # configuration
+########################################
 app = Flask(__name__)
 app.config.from_object('config.flask_config')
 mongo = PyMongo(app)
 
-
-def parse_json(f):
-    @wraps(f)
-    def decorated_function(*args, **kwargs):
-        return f(*args, **kwargs)
-    return decorated_function
-
+########################################
+#   routes
+########################################
 
 @app.route('/missing_people', methods=['POST'])
-def create_report():
-    pass
+def post_missing_people():
+    """ create a missing persons report
+
+    Creates a missing persons report in the DB.
+    """
+    return person.create_report(
+        db      = pymongo,
+        data    = request
+    )
 
 
 @app.route('/missing_people', methods=['GET'])
-def get_reports():
+def get_missing_people():
+    """ gets missing people
+
+    Returns a JSON w/ a list of report objects
+    """
     return person_controller.get_people(
         db      = pymongo,
-        data    = request.data
+        data    = request
     )
 
 
 @app.route('/missing_people', methods=['PATCH'])
 def update_report():
-    pass
+    """ update a missing persons report
+
+    Updates the status of a missing persons report
+    """
+    return person_controller.update_report(
+        db      = pymongo,
+        data    = request
+    )
 
 
 @app.route('/')
 def hello():
-    return 'hello, world', 200
+    return 'Nothing here', 400
+
 
 if __name__ == '__main__':
     app.run()
