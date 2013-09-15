9e5f38483e396b66816666f63ea9a33c38b6e842
Nate Brennand <natebrennand@gmail.com>
2013-09-15 14:10:57 -0700

    update wrapper functions

--- src/controllers/util.py
+++ src/controllers/util.py
@@ -2,18 +2,32 @@
 # libraries
 import simplejson
 
+# config
+from sys import path
+path.append('../')
+from config import errors as ERR
+path.append('controllers/')
+
+
 ####################
 #   wrappers
 ####################
 
-# looks for a param named 'data' and decodes it from JSON
-def parse_json(f):
-    @wraps(f)
-    def decorated_function(**kwargs):
-        if 'data' in kwargs:
-            kwargs['data'] = simplejson.loads(kwargs['data'])
-        return f(**kwargs)
-    return decorated_function
+# looks for a specified arguments then decodes them from JSON
+def parse_json(*parse_args):
+    def wrapped_fn(f)
+        @wraps(f)
+        def decorated_function(**kwargs):
+            for arg in parse_args:
+                if arg in kwargs:
+                    # return parsed dict parsed from JSON
+                    kwargs[arg] = simplejson.loads(kwargs[arg])
+                else:
+                    # return error
+                    return ERR.DATA_MISSING(arg)
+            return f(**kwargs)
+        return decorated_function
+    return wrapped_fn
 
 
 # checks that all "wrapper_args" are passed into the function
@@ -24,8 +38,8 @@ def check_args(**wrapper_args)
         def decorated_function(**kwargs):
             for data in wrapper_args:
                 if data not in kwargs:
-                    return wrapper_args[data]
+                    return wrapper_args[data]()
             return f(**kwargs)
         return decorated_function
-
+    return needs        
 
