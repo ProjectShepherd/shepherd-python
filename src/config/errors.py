7d7a14385d455600fbe748bc976c6e33c9e64f75
Nate Brennand <natebrennand@gmail.com>
2013-09-15 14:09:51 -0700

    fill out initial error messages

---
+++ src/config/errors.py
@@ -0,0 +1,34 @@
+
+import simplejson
+
+
+# generate an error response
+def generate_error(message=None, code=400):
+    return simplejson.dumps({
+        'message' : message,
+        'status_code' : code
+    }), code
+
+###################
+# errors
+###################
+
+def DB_MISSING():
+    return generate_error(
+        message = 'Your DB is acting up...',
+        code : 500
+    )
+
+
+def DATA_MISSING(arg=None):
+    message = 'You need to pass in paramter data'
+    if arg:
+        message = 'You need to pass in data for parameter {}'.format(arg)
+    return generate_error(
+        message = message,
+        code = 403
+    )
+
+
+
+
