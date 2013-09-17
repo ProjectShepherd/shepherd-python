3883fa5af783a4ea6049cd0cfeb5b12211534502
Nate Brennand <natebrennand@gmail.com>
2013-09-17 02:35:02 -0400

    add web frontend through submdule

--- src/run.sh
+++ src/run.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
-source config/settings.sh
+# compile Coffeescript
+cd frontend
+grunt
+cd ..
+
+# setup env for python
 cd ..
 source bin/activate
+source src/config/settings.sh
 
+# start app
 python src/app.py
 
 
